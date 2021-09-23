94fceae783dd38271c7a6b5500bfee65b88f621e
mozinomer <mozinomer.codingpixel@gmail.com>
2021-09-23 17:16:01 +0500

    made the dashboard screen

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    $('#example').DataTable();
-} );
+    // $('#example').DataTable();
+    $('li.hasChildren > a').click( function(e) {
+    	e.preventDefault();
+    	$(this).toggleClass('active');
+    	$(this).parent().find('.submenuSideBar').slideToggle();
+    })
+    $('.profiler').click( function(e) {
+    	e.preventDefault();
+    	$(this).toggleClass('active');
+    	$(this).parent().find('.submenuProfiler').slideToggle('fast','swing');
+    })
+});
